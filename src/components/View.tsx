ab1232bd8afc0decbed8406ba1f807b335f11c04
Diego Avola <davola@underscreen.com>
2023-10-13 12:45:21 -0300

    Home fix

--- src/components/View.tsx
+++ src/components/View.tsx
@@ -1,12 +1,24 @@
+import Home from "./views/Home";
+
 type ViewProps = {
     view: string;
 }
 
 export default function View({view}: ViewProps) {
-    return (
+    let currentView = (
         <div>
-            <h2>Current VIEW is</h2>
+            <h2>Default view in use is:</h2>
             <h1>{view}</h1>
+        </div>
+    );
+    switch (view) {
+        case 'home': currentView = <Home />; break;
+        case 'contact': currentView = <Home />; break;
+    }
+
+    return (
+        <div className={'view-' + view}>
+            {currentView}
         </div>
     );
 }
